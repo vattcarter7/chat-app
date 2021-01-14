2706b06ebaa55bf9fc47da051d25f2c0460ba9c4
vattcarter7 <vattcarter@gmail.com>
2021-01-14 16:18:49 +0700

    added .env file

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const config = require('./config/app');
+
+require('dotenv').config();
+
 const app = express();
 
-const port = 3000;
+const port = config.appPort;
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
